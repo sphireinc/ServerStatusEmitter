fa26cd4d6ea7c0c88e7a40a268b2a0346d900fc8
jsanc623 <juan.sanchez@juanleonardosanchez.com>
2015-02-11 16:27:57 -0500

    remove while loop in main (cpu hog), moving to a cron based sys

--- lib/transport.py
+++ lib/transport.py
@@ -9,12 +9,12 @@ class Transport():
                             datefmt='%H:%M:%S',
                             level=logging.INFO)
 
-        logging.info("Running Urban Planning")
-        logger = logging.getLogger('urbanGUI')
+        logging.info("Start transport")
 
         http = Http()
-        url = config['host'] + ((":" + str(config['port'])) if config['port'] else "") + config['endpoint']
+        url = config['host'] + ((":" + str(config['port'])) if config['port'] else "")
         resp, content = http.request(url, "POST", payload)
-        resp = resp['status'] or '', resp['content-length'] or '', resp['transfer-encoding'] or \
-               '', resp['server'] or '', resp['date'] or '', resp['content-type'] or ''
-        logger.info(resp)
+        resp = [resp['status'] or '', resp['content-length'] or '', resp['transfer-encoding'] or \
+               '', resp['server'] or '', resp['date'] or '', resp['content-type'] or '']
+
+        logging.info("End transport: " + resp)
--- main.py
+++ main.py
@@ -5,46 +5,39 @@ import json
 import time
 from lib import cpu, memory, disks, network, system, transport
 
-def main_loop():
+def main():
     config = (json.loads(open("config.json").read()))['mothership']
-    print config
-    cpu.CPU.cpu_count()
-    last_run = time.time()
 
-    while 1:
-        cur_time = int(time.time())
-        if cur_time - last_run > config['schedule']:
-            payload = {
-                "cpu": [
-                    cpu.CPU.cpu_count(),
-                    cpu.CPU.cpu_percent(),
-                    cpu.CPU.cpu_times()
-                ],
-                "memory": [
-                    memory.Memory.swap_memory(),
-                    memory.Memory.virtual_memory()
-                ],
-                "disks": [
-                    disks.Disks.disk_io_counters(),
-                    disks.Disks.disk_partitions(),
-                    disks.Disks.disk_usage()
-                ],
-                "network": [
-                    network.Network.net_connections(),
-                    network.Network.net_io_counters()
-                ],
-                "system": [
-                    system.System.users(),
-                    system.System.boot_time()
-                ]
-            }
-            payload = json.dumps(payload)
-            transport.Transport(payload, config)
-            last_run = cur_time
+    payload = {
+        "cpu": [
+            cpu.CPU.cpu_count(),
+            cpu.CPU.cpu_percent(),
+            cpu.CPU.cpu_times()
+        ],
+        "memory": [
+            memory.Memory.swap_memory(),
+            memory.Memory.virtual_memory()
+        ],
+        "disks": [
+            disks.Disks.disk_io_counters(),
+            disks.Disks.disk_partitions(),
+            disks.Disks.disk_usage()
+        ],
+        "network": [
+            network.Network.net_connections(),
+            network.Network.net_io_counters()
+        ],
+        "system": [
+            system.System.users(),
+            system.System.boot_time()
+        ]
+    }
+    payload = json.dumps(payload)
+    transport.Transport(payload, config)
 
 if __name__ == '__main__':
     try:
-        main_loop()
+        main()
     except KeyboardInterrupt:
         print >> sys.stderr, '\nExiting by user request.\n'
-        sys.exit(0)
+        sys.exit(1)
