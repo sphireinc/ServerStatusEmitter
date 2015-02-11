38c44f6140c2eb2d2ceb482809f5fe252ea5549a
jsanc623 <juan.sanchez@juanleonardosanchez.com>
2015-02-11 16:30:25 -0500

    update transport to use .get() for list keys

--- lib/transport.py
+++ lib/transport.py
@@ -3,18 +3,16 @@ import logging
 
 class Transport():
     def __init__(self, payload, config):
-        logging.basicConfig(filename="/var/log/sse.log",
-                            filemode='a',
+        logging.basicConfig(filename="/var/log/sse.log", filemode='a',
                             format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
-                            datefmt='%H:%M:%S',
-                            level=logging.INFO)
-
+                            datefmt='%H:%M:%S', level=logging.INFO)
         logging.info("Start transport")
 
         http = Http()
         url = config['host'] + ((":" + str(config['port'])) if config['port'] else "")
         resp, content = http.request(url, "POST", payload)
-        resp = [resp['status'] or '', resp['content-length'] or '', resp['transfer-encoding'] or \
-               '', resp['server'] or '', resp['date'] or '', resp['content-type'] or '']
+        resp = [resp.get('status', ''), resp.get('content-length', ''),
+                resp.get('transfer-encoding', ''), resp.get('server', ''), 
+                resp.get('date', ''), resp.get('content-type', '')]
 
         logging.info("End transport: " + resp)
