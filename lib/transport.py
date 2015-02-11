1ad54c50391a08ca7d50cc6ab6bcac70d3d7043f
jsanc623 <juan.sanchez@juanleonardosanchez.com>
2015-02-11 16:35:53 -0500

    list > dict

--- lib/transport.py
+++ lib/transport.py
@@ -12,9 +12,12 @@ class Transport():
         url = config['host'] + ((":" + str(config['port'])) if config['port'] else "")
         print url
         resp, content = http.request(url, "POST", payload)
-        resp = [resp.get('status', ''), resp.get('content-length', ''),
-                resp.get('transfer-encoding', ''), resp.get('server', ''), 
-                resp.get('date', ''), resp.get('content-type', '')]
+        resp = {"status":               resp.get('status', ''), 
+                "content-length":       resp.get('content-length', ''),
+                "transfer-encoding":    resp.get('transfer-encoding', ''), 
+                "server":               resp.get('server', ''), 
+                "date":                 resp.get('date', ''), 
+                "content-type":         resp.get('content-type', '')}
         print resp
 
         logging.info("End transport: " + str(resp))
