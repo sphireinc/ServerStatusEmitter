ab2d6483379ac0d2291aa3ff1c187b868e462f30
jsanc623 <juan.sanchez@juanleonardosanchez.com>
2015-02-13 16:21:21 -0500

    udp testing

--- lib/transport.py
+++ lib/transport.py
@@ -1,16 +1,24 @@
-import requests
+import socket
 
 class Transport():
     def __init__(self, payload, config, logger):
-        logger.info("Start transport")
+#        logger.info("Start transport")
+        print "Send"
 
-        url = config['host'] + ((":" + str(config['port'])) if config['port'] else "")
-        response = requests.post(url, data=payload)
+        #url = config['host'] + ((":" + str(config['port'])) if config['port'] else "")
+        #response = requests.post(url, data=payload)
 
-        response = { "status_code": response.status_code,
-                     "encoding": response.encoding,
-                     "headers": response.headers,
-                     "content": response.text
-                   }
+        payload = str(payload)
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        print sock, config
+#        sock.setblocking(0)
+        sock.sendto(payload, (config['host'], config['port']))
 
-        logger.info("End transport: " + str(response))
+        #response = { "status_code": response.status_code,
+        #             "encoding": response.encoding,
+        #             "headers": response.headers,
+        #             "content": response.text
+        #           }
+
+        #logger.info("End transport: " + str(response))
+#        logger.info("End transport")
