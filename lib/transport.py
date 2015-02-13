1be1d96d19aa994a7b5fd64fe51db820ba4cf183
jsanc623 <juan.sanchez@juanleonardosanchez.com>
2015-02-13 17:06:19 -0500

    remove commented code

--- lib/transport.py
+++ lib/transport.py
@@ -2,22 +2,7 @@ import socket
 
 class Transport():
     def __init__(self, payload, config, sock):
-#        logger.info("Start transport")
-        print "Send"
-
-        #url = config['host'] + ((":" + str(config['port'])) if config['port'] else "")
-        #response = requests.post(url, data=payload)
-
         payload = str(payload)
-#        sock.setblocking(0)
+        sock.setblocking(0)
         sock.sendto(payload, (config.get('mothership').get('host'), 
                               config.get('mothership').get('port')))
-
-        #response = { "status_code": response.status_code,
-        #             "encoding": response.encoding,
-        #             "headers": response.headers,
-        #             "content": response.text
-        #           }
-
-        #logger.info("End transport: " + str(response))
-#        logger.info("End transport")
