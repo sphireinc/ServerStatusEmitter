3bd0af07567142fea7a964230c62ac43b1e8af7a
juan.sanchez <juan.sanchez@borderfree.com>
2015-02-24 15:55:01 -0500

    update grep clause in default-install.sh

--- default-install.sh
+++ default-install.sh
@@ -1,21 +1,21 @@
 # Install python2.7 if not installed
-PKG_OK=$(dpkg-query -W --showformat='${Status}\n' python2.7|grep "Python2.7 installed, not forcing install")
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' python2.7|grep "install ok installed")
 if [ "" == "$PKG_OK" ]; then
-    echo "Installing Python2.7"
+    echo "Installing python2.7"
     apt-get --force-yes --yes install python2.7
 fi;
 
 # Install python-dev if not installed
-PKG_OK=$(dpkg-query -W --showformat='${Status}\n' python-dev|grep "Python-dev installed, not forcing install")
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' python-dev|grep "install ok installed")
 if [ "" == "$PKG_OK" ]; then
-    echo "Installing Python-dev"
+    echo "Installing python-dev"
     apt-get --force-yes --yes install python-dev
 fi;
 
 # Install python-pip if not installed
-PKG_OK=$(dpkg-query -W --showformat='${Status}\n' python-pip|grep "Python-pip installed, not forcing install")
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' python-pip|grep "install ok installed")
 if [ "" == "$PKG_OK" ]; then
-    echo "Installing Python-pip"
+    echo "Installing python-pip"
     apt-get --force-yes --yes install python-pip
 fi;
 
