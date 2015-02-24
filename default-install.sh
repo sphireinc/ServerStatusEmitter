681ab2a6ed3298f11f4f9fafa21f2e14f530d0a0
jsanc623 <juan.sanchez@juanleonardosanchez.com>
2015-02-24 16:00:10 -0500

    defualt-install.sh cleanup

--- default-install.sh
+++ default-install.sh
@@ -3,33 +3,53 @@ PKG_OK=$(dpkg-query -W --showformat='${Status}\n' python2.7|grep "install ok ins
 if [ "" == "$PKG_OK" ]; then
     echo "Installing python2.7"
     apt-get --force-yes --yes install python2.7
+else
+    echo "Detected python2.7 installed already"
 fi;
+
+echo ""
 
 # Install python-dev if not installed
 PKG_OK=$(dpkg-query -W --showformat='${Status}\n' python-dev|grep "install ok installed")
 if [ "" == "$PKG_OK" ]; then
     echo "Installing python-dev"
     apt-get --force-yes --yes install python-dev
+else
+    echo "Detected python-dev installed already"
 fi;
+
+echo ""
 
 # Install python-pip if not installed
 PKG_OK=$(dpkg-query -W --showformat='${Status}\n' python-pip|grep "install ok installed")
 if [ "" == "$PKG_OK" ]; then
     echo "Installing python-pip"
     apt-get --force-yes --yes install python-pip
+else
+    echo "Detected python-pip installed already"
 fi;
 
+echo ""
+
 # Make the log directory for supervisord if not exists
-if [ -d "/var/log/supervisord" && ! -L "/var/log/supervisord" ]; then
+if [[ -d "/var/log/supervisord" && ! -L "/var/log/supervisord" ]]; then
     echo "Creating supervisord log directory"
     mkdir /var/log/supervisord
+else
+    echo "Detected supervisord log directory already exists."
 fi
+
+echo ""
 
 # Make the log file for supervisord if not exists
 if [ -f "/var/log/supervisord/supervisord.log" ]; then
     echo "Creating supervisord log file in supervisord log directory"
     touch /var/log/supervisord/supervisord.log
+else
+    echo "Detected supervisord log file in supervisord log directory already exists."
 fi
+
+echo ""
 
 # Install any requirements
 echo "Installing project requirements via pip"
