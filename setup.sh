a1e15da02f712bd3682d5488123d08f438f42b7d
juan.sanchez <juan.sanchez@borderfree.com>
2015-03-02 10:58:08 -0500

    Simplifies the bash script using an array for the packages

--- setup.sh
+++ setup.sh
@@ -1,46 +1,21 @@
-# Install supervisor if not installed
-PKG_OK=$(dpkg-query -W --showformat='${Status}\n' supervisor|grep "install ok installed")
-if [ "" == "$PKG_OK" ]; then
-    echo "Installing supervisor"
-    apt-get --force-yes --yes install supervisor
-else
-    echo "Detected supervisor installed already"
-fi;
+#!/bin/sh
 
-echo ""
+packages=( supervisor python2.7 python-dev python-pip )
 
-# Install python2.7 if not installed
-PKG_OK=$(dpkg-query -W --showformat='${Status}\n' python2.7|grep "install ok installed")
-if [ "" == "$PKG_OK" ]; then
-    echo "Installing python2.7"
-    apt-get --force-yes --yes install python2.7
-else
-    echo "Detected python2.7 installed already"
-fi;
+# Iterate over packages, check if they're installed, if not install
+for i in "${packages[@]}"
+do
+    :
+    PKG_OK=$(dpkg-query -W --showformat='${Status}\n' ${i}|grep "install ok installed")
+    if [ "" == "$PKG_OK" ]; then
+        echo "Installing ${i}"
+        apt-get --force-yes --yes install ${i}
+    else
+        echo "Detected ${i} installed already"
+    fi;
 
-echo ""
-
-# Install python-dev if not installed
-PKG_OK=$(dpkg-query -W --showformat='${Status}\n' python-dev|grep "install ok installed")
-if [ "" == "$PKG_OK" ]; then
-    echo "Installing python-dev"
-    apt-get --force-yes --yes install python-dev
-else
-    echo "Detected python-dev installed already"
-fi;
-
-echo ""
-
-# Install python-pip if not installed
-PKG_OK=$(dpkg-query -W --showformat='${Status}\n' python-pip|grep "install ok installed")
-if [ "" == "$PKG_OK" ]; then
-    echo "Installing python-pip"
-    apt-get --force-yes --yes install python-pip
-else
-    echo "Detected python-pip installed already"
-fi;
-
-echo ""
+    echo ""
+done
 
 echo "Copying SSE_Python_supervisord.conf to /etc/supervisor/conf.d"
 cp SSE_Python_supervisord.conf /etc/supervisor/conf.d/SSE_Python_supervisord.conf
