87226db751178b8cb4a93792c28b677b4a5be798
jsanc623 <juan.sanchez@juanleonardosanchez.com>
2015-03-06 14:48:43 -0500

    add file write capability to server.go

--- server.go
+++ server.go
@@ -1,50 +1,56 @@
 package main
 
 import (
-    "fmt"
-    "net"
-    "time"
+	"fmt"
+	"os"
+	"net"
+	"time"
 )
 
 func main() {
-    port := ":1336"
-    udpAddress, err := net.ResolveUDPAddr("udp4", port)
+	port := ":1336"
+	udpAddress, err := net.ResolveUDPAddr("udp4", port)
 
-    if err != nil {
-        fmt.Println("error resolving UDP address on ", port)
-        fmt.Println(err)
-        return
-    }
+	if err != nil {
+		fmt.Println("error resolving UDP address on ", port)
+		fmt.Println(err)
+		return
+	}
 
-    conn, err := net.ListenUDP("udp", udpAddress)
+	conn, err := net.ListenUDP("udp", udpAddress)
 
-    if err != nil {
-        fmt.Println("error listening on UDP port ", port)
-        fmt.Println(err)
-        return
-    }
+	if err != nil {
+		fmt.Println("error listening on UDP port ", port)
+		fmt.Println(err)
+		return
+	}
 
-    defer conn.Close()
-    var buf []byte = make([]byte, 1318080)
+	defer conn.Close()
+	var buf []byte = make([]byte, 1318080)
 
-    for {
-        time.Sleep(100 * time.Millisecond)
-        n, address, err := conn.ReadFromUDP(buf)
+	f, _ := os.Create("/opt/sse/responses.log")
+	defer f.Close()
 
-        if err != nil {
-            fmt.Println("error reading data from connection")
-            fmt.Println(err)
-            return
-        }
+	for {
+		time.Sleep(100 * time.Millisecond)
+		n, address, err := conn.ReadFromUDP(buf)
 
-        if address != nil {
-            fmt.Println("got message from ", address, " with n = ", n)
+		if err != nil {
+			fmt.Println("error reading data from connection")
+			fmt.Println(err)
+			return
+		}
 
-            if n > 0 {
-                fmt.Println("from address", address, "got message:", string(buf[0:n]), n)
-            }
-        }
-    }
+		if address != nil {
+			fmt.Println("got message from ", address, " with n = ", n)
+
+			if n > 0 {
+				f.Write([]byte(buf[0:n]))
+                f.Write([]byte("\n"))
+				f.Sync()
+				fmt.Println("from address", address, "got message:", string(buf[0:n]), n)
+			}
+		}
+	}
 
 }
-
