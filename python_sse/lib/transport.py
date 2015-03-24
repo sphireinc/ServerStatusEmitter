50e3ba98cd8caccb8c66bdd301e3328a33562017
jsanc623 <juan.sanchez@juanleonardosanchez.com>
2015-03-24 13:20:17 -0400

    remove python version

--- python_sse/lib/transport.py
+++
@@ -1,7 +0,0 @@
-class Transport():
-    def __init__(self, payload, config, sock):
-        payload = str(payload)
-
-        sock.setblocking(0)
-        sock.sendto(payload, (config.get('mothership').get('host'),
-                              config.get('mothership').get('port')))
